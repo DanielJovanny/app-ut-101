539655c59f2a628887c5da8b3564a712b6271470
Daniel Puc <skar.dpm@gmail.com>
2024-03-01 19:08:16 -0500

    feat:layout andfood component

---
+++ src/app/foods/food-list/food-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../shared/food.service';
+import { Food } from '../shared/food.model';
+import { FoodComponent } from '../food/food.component';
+
+@Component({
+  selector: 'app-food-list',
+  standalone: true,
+  imports: [FoodComponent],
+  templateUrl: './food-list.component.html',
+  styleUrl: './food-list.component.scss'
+})
+export class FoodListComponent implements OnInit {
+
+  //Variable
+  data:Food[]= [];
+  constructor(public serviceFood:FoodService){
+          
+  }
+
+
+  ngOnInit(): void {
+    this.data = this.serviceFood.getAllFoods();
+    console.log(this.data);
+    
+  }
+
+
+
+}
