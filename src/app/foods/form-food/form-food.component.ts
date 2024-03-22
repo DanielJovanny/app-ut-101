234691fca20e320426ce1323d45c3f962d527836
Daniel Puc <skar.dpm@gmail.com>
2024-03-22 18:25:59 -0500

    feat: formulario añadir y actualizar comida

--- src/app/foods/form-food/form-food.component.ts
+++ src/app/foods/form-food/form-food.component.ts
@@ -1,35 +1,143 @@
-import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButton } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { Food } from '../shared/food.model';
+import { FoodService } from '../shared/food.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-food',
   standalone: true,
-  imports: [ReactiveFormsModule, MatIconModule,MatInputModule,MatFormFieldModule, MatButton],
+  imports: [
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButton,
+    MatSelectModule,
+  ],
   templateUrl: './form-food.component.html',
-  styleUrl: './form-food.component.scss'
+  styleUrl: './form-food.component.scss',
 })
-export class FormFoodComponent {
+export class FormFoodComponent implements OnInit {
+  form = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    description: ['', [Validators.required, Validators.minLength(20)]],
+    image: ['', [Validators.required]],
+    category: ['', [Validators.required]],
+    price: ['', [Validators.required, Validators.min(2)]],
+  });
 
-  
-  name = new FormControl([Validators.maxLength(20),Validators.required]);
+  constructor(
+    private formBuilder: FormBuilder,
+    public serviceFood: FoodService,
+    public router: Router
+  ) {}
 
-  public showValue(){
-      if (this.name.hasError('required')) {
-        return 'You must enter a value';
-      } else if (this.name.hasError('maxLength')) {
-        return 'Minimo 20 caracteres'
+  fooId: number = -1;
+  edit: boolean = false;
+  activedRoute: ActivatedRoute = inject(ActivatedRoute);
+  food?:Food = {
+    name:'',
+    descripcion:'',
+    category:'',
+    image:'',
+    price:0
+  }
 
+  ngOnInit(): void {
+    if (this.activedRoute.snapshot.params['id']) {
+      this.edit = true;
+      this.fooId = Number(this.activedRoute.snapshot.params['id']);
+      console.log(this.fooId);
+      this.food = this.serviceFood.getOne(this.fooId);
+      if (this.food) {
+        this.form.patchValue({
+          name:this.food.name,
+          category:this.food.category,
+          description:this.food.descripcion,
+          image:this.food.image,
+          price:this.food.price.toString()
+        })
       }
-      return 'Error';
-
+      
+      
+    }
   }
 
-  public changeValue():void{
-   // this.name.setValue('Pizza de Queso')
+  public updateData(){
+    if (this.form.status == 'VALID') {
+      if (
+        this.name?.value &&
+        this.description?.value &&
+        this.image?.value &&
+        this.category?.value &&
+        this.price?.value
+      ) {
+        let priceNumber = Number(this.price.value);
+        let comida: Food = {
+          id:this.fooId,
+          name: this.name?.value,
+          descripcion: this.description?.value,
+          category: this.category?.value,
+          image: this.image?.value,
+          price: priceNumber,
+        };
+
+        console.log(comida);
+        this.serviceFood.updateFood(comida);
+        this.router.navigate(['/food/food-list']);
+      }
+    }
   }
 
+  public sendData() {
+    if (this.form.status == 'VALID') {
+      if (
+        this.name?.value &&
+        this.description?.value &&
+        this.image?.value &&
+        this.category?.value &&
+        this.price?.value
+      ) {
+        let priceNumber = Number(this.price.value);
+        let comida: Food = {
+          name: this.name?.value,
+          descripcion: this.description?.value,
+          category: this.category?.value,
+          image: this.image?.value,
+          price: priceNumber,
+        };
+
+        console.log(comida);
+        this.serviceFood.addFood(comida);
+        this.router.navigate(['/food/food-list']);
+      }
+    }
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+  get description() {
+    return this.form.get('description');
+  }
+  get image() {
+    return this.form.get('image');
+  }
+  get category() {
+    return this.form.get('category');
+  }
+  get price() {
+    return this.form.get('price');
+  }
 }
